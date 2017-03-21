3d4f77c46fb1705ac71b9607a355aabd5334aef0
Xingguang Wei <jxwxg@foxmail.com>
2017-03-21 23:42:41 +0800

    1. Complete the hash search function.

--- Search/hash_search.cpp
+++ Search/hash_search.cpp
@@ -1,4 +1,6 @@
-#include <iosream>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 #define HASHSIZE 12
@@ -7,10 +9,11 @@ using namespace std;
 class Hash
 {
 	public:
-		bool Hash();
+		Hash();
 		int hash_key(int key);
 		void insert_hash(int key);
-		bool search_hash(int key);
+		bool search_hash(int key, int &addr);
+		void show_hash();
 
 	private:
 		int *elem;
@@ -28,9 +31,59 @@ Hash::Hash()
 		elem[i] = NULLKEY;
 }
 
-int hash_key(int key)
+void Hash::show_hash()
+{
+	for(int i = 0; i < HASHSIZE; i++)
+		cout << i << "-> " << elem[i] << endl;
+}
+
+int Hash::hash_key(int key)
 {
 	return key % M;
 }
 
+void Hash::insert_hash(int key)
+{
+	int addr = hash_key(key);
+	while(elem[addr] != NULLKEY)
+		addr = (addr+1) % M;
 
+	elem[addr] = key;
+}
+
+bool Hash::search_hash(int key, int &addr)
+{
+	addr = hash_key(key);
+	while(elem[addr] != key)
+	{
+		addr = (addr + 1) % M;
+
+		if(elem[addr] == NULLKEY || addr == hash_key(key))
+			return false;
+	}
+
+	return true;
+}
+
+int main(int argc, char **argv)
+{
+	Hash *hs = new Hash;
+
+	srand(unsigned(time(0)));
+
+	for(int i = 0; i < HASHSIZE; i++)
+		hs->insert_hash(rand() % 20);
+
+	hs->show_hash();
+
+	int addr;
+	int key = 10;
+
+	if( hs->search_hash(key, addr) )
+	{
+		cout << "The key " << key << " is in: " << addr << endl;
+	}
+
+	delete hs;
+	hs = NULL;
+}
